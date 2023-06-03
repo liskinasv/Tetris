a9516a39414dadb7258d1c86c1a39ce28032f04d
Svetlana Liskina <liskinasv@yandex.ru>
2023-06-03 17:56:29 +0300

    Вывели 2 точки на экран

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.SetWindowSize(40, 30);
+Console.SetBufferSize(40, 30);
+
+
+Draw(2, 3, '*');
+Draw(5, 6, '#');
+
+
+
+static void Draw(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(c);
+}
