43e49344363d9250452236079894d0ed1c8ecb1b
Svetlana Liskina <liskinasv@yandex.ru>
2023-06-03 19:10:03 +0300

    Добавлен класс Point

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,16 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Drawing;
+
 Console.SetWindowSize(40, 30);
 Console.SetBufferSize(40, 30);
 
 
-Draw(2, 3, '*');
-Draw(5, 6, '#');
 
-
-
-static void Draw(int x, int y, char c)
-{
-    Console.SetCursorPosition(x, y);
-    Console.Write(c);
-}
+Point p1 = new Point();
+Point p2 = new Point();
